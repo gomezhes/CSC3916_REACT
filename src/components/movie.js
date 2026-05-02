f98d4e43a6f544d1d4b12690d0a649dbebf98d8f
gomezhes <sony.gomezhernandez@ucdenver.edu>
2026-05-01 21:43:55 -0600

    revert

--- src/components/movie.js
+++ src/components/movie.js
@@ -6,8 +6,17 @@ import MovieDetail from "../components/moviedetail"
 
 // support routing
 
-function Movie() {
-    return (<MovieDetail/>)
+function Movie(props) {
+    const [selectedMovie] = useState(props.selectedMovie);
+    const params = useParams();
+    const movieId = params.movieId;
+    console.log(movieId);
+    const dispatch = useDispatch();
+    if (selectedMovie == null) {
+        dispatch(fetchMovie(movieId));
+    }
+
+    return (<MovieDetail movieId={movieId} />)
 }
 
 export default Movie;
