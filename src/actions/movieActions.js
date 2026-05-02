f3b4c98c721c88d8de039fc3623a8d109a663b99
gomezhes <sony.gomezhernandez@ucdenver.edu>
2026-05-01 20:42:14 -0600

    update

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -70,3 +70,27 @@ export function fetchMovies() {
         }).catch((e) => console.log(e));
     }
 }
+
+export function submitReview(movieId, reviewDetails) {
+    return dispatch => {
+        return fetch('${env.REACT_APP_API_URL}/reviews', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                movieId: movieId,
+                rating: reviewDetails.rating,
+                review: reviewDetails.review
+            }),
+            mode: 'cors'
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText)
+            }
+            dispatch(fetchMovie(movieId));
+        }).catch((e) => console.log(e))
+    }
+}
